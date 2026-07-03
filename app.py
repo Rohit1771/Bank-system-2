ff3f10e7406d1182b4657f2ff79c6cf2ffdb9d9d
Rohit1771 <kharaderohit45891@gmail.com>
2026-07-03 14:19:27 +0530

    Added SQLite login and login success message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask,render_template, request, flash
+from flask import Flask,render_template, request, flash, redirect, url_for 
 
 app = Flask(__name__)
 app.secret_key = "rohit123"
@@ -8,9 +8,28 @@ app.secret_key = "rohit123"
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login",
+          methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        conn = sqlite3.connect("bank.db")
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM USERS WHERE username = ? AND password = ?", (username,password))
+        user = cursor.fetchone()
+        conn.close()
+        if user:
+            flash("Login successful", "success")
+            return redirect(url_for("login"))
+        else:
+            flash("Invalid username or password", "error")
+            return redirect(url_for("login"))
     return render_template("login.html")
+    
+        
 
 @app.route("/register",
            methods=["GET", "POST"])
@@ -20,15 +39,20 @@ def register():
         email = request.form.get("email")
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
+
         if password != confirm_password:
-            return "Passwords do not match"
+            flash("Passwords do not match", "error")
+            return render_template("register.html")
+        
         conn = sqlite3.connect("bank.db")
         cursor = conn.cursor()
         
         cursor.execute("SELECT email FROM users WHERE email = ?", (email,))
         existing_user = cursor.fetchone()
         if existing_user:
-            return "User with this email already exists"
+            flash("User with this email already exists", "error")
+            return render_template("register.html")
+        
         cursor.execute("SELECT username FROM users WHERE username = ?", (username,))
         existing_username = cursor.fetchone()
         if existing_username:
@@ -41,6 +65,6 @@ def register():
         conn.close()
         
         flash("User registered successfully", "success")    
-    return render_template("register.html")
+        return render_template("register.html")
 
 app.run(debug=True)
